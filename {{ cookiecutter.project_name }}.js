4fe93a22ca0670e24e16da15ae789ffb7ea10a46
David Betschart <betschart@allink.ch>
2015-01-07 19:08:08 +0100

    initial file commit

---
+++ {{ cookiecutter.project_name }}.js
@@ -0,0 +1,17 @@
+/*
+
+author: {{ cookiecutter.author_full_name }}
+website: {{ cookiecutter.author_website }}
+version: {{ cookiecutter.version }}
+
+{{ cookiecutter.project_short_description }}
+
+*/
+;(function() {
+    'use strict';
+
+    var {{ cookiecutter.project_name }} = function() {
+
+    }
+
+}());
